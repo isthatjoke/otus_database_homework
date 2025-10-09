09e10c99819a4de276d67fb6d7e5ea31c3af5ce0
root <root@mail.rucrocuv058d.croc.oneabbott.com>
2025-10-09 12:16:28 +0300

    code refacor

--- 18_lesson/18_lesson.sql
+++ 18_lesson/18_lesson.sql
@@ -33,89 +33,12 @@ CREATE TABLE good_sum_mart
 	sum_sale	numeric(16, 2)NOT NULL
 );
 
--- Функция для первичного насыщения таблицы в условиях одинаковых имен товаров
-CREATE OR REPLACE FUNCTION begin_update_good_sum_mart()
-RETURNS void
-AS
-$BODY$
-BEGIN
-    INSERT INTO good_sum_mart (good_name, sum_sale)
-    SELECT G.good_name, (G.good_price * S.sales_qty)
-    FROM goods G
-    INNER JOIN sales S ON S.good_id = G.goods_id;
-END;
-$BODY$
-LANGUAGE plpgsql;
-
--- запускаем функцию
-SELECT begin_update_good_sum_mart();
-
--- проверяем, что в таблице
-SELECT begin_update_good_sum_mart();
-
--- Триггер + функция при изменении продажи
-CREATE OR REPLACE FUNCTION tf_sale_update()
-  RETURNS TRIGGER
-  AS
-  $BODY$
-  DECLARE
-  v_new_sale_amount NUMERIC(16,2);
-  v_good_name VARCHAR(63);
-  v_old_sale_amount NUMERIC(16,2);
-
-  BEGIN
-
-  SELECT G.good_price * OLD.sales_qty
-    INTO v_old_sale_amount
-    FROM goods G
-    WHERE G.goods_id = OLD.good_id;
-
-  SELECT G.good_name, G.good_price * NEW.sales_qty
-    INTO v_good_name, v_new_sale_amount
-    FROM goods G
-    WHERE G.goods_id = NEW.good_id;
-
-  UPDATE good_sum_mart
-    SET sum_sale = v_new_sale_amount
-    WHERE good_name = v_good_name
-    AND sum_sale = v_old_sale_amount;
-
-    RETURN NEW;
-  END;
-  $BODY$
-  LANGUAGE plpgsql;
-
--- Триггер + функция при удалении продажи
-CREATE OR REPLACE FUNCTION tf_sale_canceled()
-  RETURNS TRIGGER
-  AS
-  $BODY$
-  DECLARE
-    v_old_good_name VARCHAR(63);
-    v_old_sale_amount NUMERIC(16,2);
-
-  BEGIN
-
-    SELECT G.good_name, G.good_price * OLD.sales_qty
-      INTO v_old_good_name, v_old_sale_amount
-      FROM goods G
-      WHERE G.goods_id = OLD.good_id;
-
-    DELETE FROM good_sum_mart
-      WHERE good_name = v_old_good_name
-      AND sum_sale = v_old_sale_amount;
-
-    RETURN OLD;
-END;
-$BODY$
-LANGUAGE plpgsql;
-
-CREATE TRIGGER tr_sale_canceled
-AFTER DELETE
-ON sales
-FOR EACH ROW
-EXECUTE FUNCTION tf_sale_canceled();
-
+-- Первичное заполнение таблицы good_sum_mart
+INSERT INTO good_sum_mart (good_name, sum_sale)
+SELECT G.good_name, sum(G.good_price * S.sales_qty)
+FROM goods G
+INNER JOIN sales S ON S.good_id = G.goods_id
+GROUP BY G.good_name;
 
 -- Триггер + функция при новой продаже
 CREATE OR REPLACE FUNCTION tf_sale_added()
@@ -134,8 +57,9 @@ CREATE OR REPLACE FUNCTION tf_sale_added()
       WHERE G.goods_id = NEW.good_id;
 
 
-    INSERT INTO good_sum_mart (good_name, sum_sale)
-    VALUES (v_good_name, v_sale_amount);
+    UPDATE good_sum_mart
+    SET sum_sale = (sum_sale + v_sale_amount)
+    WHERE good_name = v_good_name;
     
     
     RETURN NEW;
@@ -151,8 +75,201 @@ FOR EACH ROW
 EXECUTE FUNCTION tf_sale_added();
 
 
+-- Триггер + функция при изменении продажи
+CREATE OR REPLACE FUNCTION tf_sale_update()
+  RETURNS TRIGGER
+  AS
+  $BODY$
+  DECLARE
+  v_new_sale_amount NUMERIC(16,2);
+  v_good_name VARCHAR(63);
+  v_old_sale_amount NUMERIC(16,2);
+  v_sum_diff NUMERIC(16,2);
 
--- Проверки
-INSERT INTO sales (good_id, sales_qty) VALUES (1, 70);
-UPDATE sales SET sales_qty = 69 WHERE sales_id = 5;
-DELETE FROM sales WHERE sales_id = 5;
+  BEGIN
+
+  SELECT G.good_price * OLD.sales_qty
+    INTO v_old_sale_amount
+    FROM goods G
+    WHERE G.goods_id = OLD.good_id;
+  RAISE NOTICE 'старая сумма %', v_old_sale_amount;
+
+  SELECT G.good_name, G.good_price * NEW.sales_qty
+    INTO v_good_name, v_new_sale_amount
+    FROM goods G
+    WHERE G.goods_id = NEW.good_id;
+  RAISE NOTICE 'новая сумма %, имя %', v_new_sale_amount, v_good_name;
+
+  v_sum_diff := v_new_sale_amount - v_old_sale_amount;
+  RAISE NOTICE 'разница сумм %', v_sum_diff;
+
+  UPDATE good_sum_mart
+    SET sum_sale = (sum_sale + v_sum_diff)
+    WHERE good_name = v_good_name;
+
+    RETURN NEW;
+  END;
+  $BODY$
+  LANGUAGE plpgsql;
+
+CREATE TRIGGER tr_sale_update
+AFTER UPDATE
+ON sales
+FOR EACH ROW
+EXECUTE FUNCTION tf_sale_update();
+
+-- Триггер + функция при удалении продажи
+CREATE OR REPLACE FUNCTION tf_sale_canceled()
+  RETURNS TRIGGER
+  AS
+  $BODY$
+  DECLARE
+    v_good_name VARCHAR(63);
+    v_old_sale_amount NUMERIC(16,2);
+
+  BEGIN
+
+    SELECT G.good_name, G.good_price * OLD.sales_qty
+      INTO v_good_name, v_old_sale_amount
+      FROM goods G
+      WHERE G.goods_id = OLD.good_id;
+
+    UPDATE good_sum_mart
+      SET sum_sale = (sum_sale - v_old_sale_amount)
+      WHERE good_name = v_good_name;
+
+    RETURN OLD;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER tr_sale_canceled
+AFTER DELETE
+ON sales
+FOR EACH ROW
+EXECUTE FUNCTION tf_sale_canceled();
+
+
+-- -- Функция для первичного насыщения таблицы в условиях одинаковых имен товаров
+-- CREATE OR REPLACE FUNCTION begin_update_good_sum_mart()
+-- RETURNS void
+-- AS
+-- $BODY$
+-- BEGIN
+--     INSERT INTO good_sum_mart (good_name, sum_sale)
+--     SELECT G.good_name, (G.good_price * S.sales_qty)
+--     FROM goods G
+--     INNER JOIN sales S ON S.good_id = G.goods_id;
+-- END;
+-- $BODY$
+-- LANGUAGE plpgsql;
+
+-- -- запускаем функцию
+-- SELECT begin_update_good_sum_mart();
+
+-- -- проверяем, что в таблице
+-- SELECT begin_update_good_sum_mart();
+
+-- -- Триггер + функция при изменении продажи
+-- CREATE OR REPLACE FUNCTION tf_sale_update()
+--   RETURNS TRIGGER
+--   AS
+--   $BODY$
+--   DECLARE
+--   v_new_sale_amount NUMERIC(16,2);
+--   v_good_name VARCHAR(63);
+--   v_old_sale_amount NUMERIC(16,2);
+
+--   BEGIN
+
+--   SELECT G.good_price * OLD.sales_qty
+--     INTO v_old_sale_amount
+--     FROM goods G
+--     WHERE G.goods_id = OLD.good_id;
+
+--   SELECT G.good_name, G.good_price * NEW.sales_qty
+--     INTO v_good_name, v_new_sale_amount
+--     FROM goods G
+--     WHERE G.goods_id = NEW.good_id;
+
+--   UPDATE good_sum_mart
+--     SET sum_sale = v_new_sale_amount
+--     WHERE good_name = v_good_name
+--     AND sum_sale = v_old_sale_amount;
+
+--     RETURN NEW;
+--   END;
+--   $BODY$
+--   LANGUAGE plpgsql;
+
+-- -- Триггер + функция при удалении продажи
+-- CREATE OR REPLACE FUNCTION tf_sale_canceled()
+--   RETURNS TRIGGER
+--   AS
+--   $BODY$
+--   DECLARE
+--     v_old_good_name VARCHAR(63);
+--     v_old_sale_amount NUMERIC(16,2);
+
+--   BEGIN
+
+--     SELECT G.good_name, G.good_price * OLD.sales_qty
+--       INTO v_old_good_name, v_old_sale_amount
+--       FROM goods G
+--       WHERE G.goods_id = OLD.good_id;
+
+--     DELETE FROM good_sum_mart
+--       WHERE good_name = v_old_good_name
+--       AND sum_sale = v_old_sale_amount;
+
+--     RETURN OLD;
+-- END;
+-- $BODY$
+-- LANGUAGE plpgsql;
+
+-- CREATE TRIGGER tr_sale_canceled
+-- AFTER DELETE
+-- ON sales
+-- FOR EACH ROW
+-- EXECUTE FUNCTION tf_sale_canceled();
+
+
+-- -- Триггер + функция при новой продаже
+-- CREATE OR REPLACE FUNCTION tf_sale_added()
+--   RETURNS TRIGGER
+--   AS
+--   $BODY$
+--   DECLARE
+--     v_good_name VARCHAR(63);
+--     v_sale_amount NUMERIC(16,2);
+  
+--   BEGIN
+
+--     SELECT G.good_name, G.good_price * NEW.sales_qty
+--       INTO v_good_name, v_sale_amount
+--       FROM goods G
+--       WHERE G.goods_id = NEW.good_id;
+
+
+--     INSERT INTO good_sum_mart (good_name, sum_sale)
+--     VALUES (v_good_name, v_sale_amount);
+    
+    
+--     RETURN NEW;
+--   END;
+--   $BODY$
+--   LANGUAGE plpgsql;
+
+
+-- CREATE TRIGGER tr_sale_added
+-- AFTER INSERT
+-- ON sales
+-- FOR EACH ROW
+-- EXECUTE FUNCTION tf_sale_added();
+
+
+
+-- -- Проверки
+-- INSERT INTO sales (good_id, sales_qty) VALUES (1, 70);
+-- UPDATE sales SET sales_qty = 69 WHERE sales_id = 5;
+-- DELETE FROM sales WHERE sales_id = 5;
